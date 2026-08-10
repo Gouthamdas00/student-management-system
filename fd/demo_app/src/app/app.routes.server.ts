5b6ca1a3e9011f2142fc7148eb0307d4ef1b93cb
goutham.d-at-136577491522 <gouthamdas2003@gmail.com>
2026-08-10 16:54:58 +0530

    Fix prerender routes and update CSS budget

--- fd/demo_app/src/app/app.routes.server.ts
+++ fd/demo_app/src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'departments/edit/:id',
+    renderMode: RenderMode.Client // Renders on the browser when navigated to
+  },
+  {
+    path: 'departments/:id/staff',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // Keeps SSG for static routes
   }
 ];
